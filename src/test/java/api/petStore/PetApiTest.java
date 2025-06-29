9073cbc8d9f9ec545fbbc8aa5190b4a267464299
carlosalfredo.rojas <crojasw100@gmail.com>
2025-06-28 19:39:05 -0500

    develop/feature addying rest assured tests

--- src/test/java/api/petStore/PetApiTest.java
+++ src/test/java/api/petStore/PetApiTest.java
@@ -4,16 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class PetApiTest extends BaseTest{
 
     @Test
-    public void getPetById_Returns200() {
+    public void getPetByIdReturns200() {
 
-        Response response = RestAssured
-                .given()
+        Response response = given()
                 .when()
                 .get("/3")
                 .then()
@@ -25,7 +25,51 @@ public class PetApiTest extends BaseTest{
         assertThat(contentType, containsString("application/json"));
 
         int petId = response.jsonPath().getInt("id");
-        assertThat(petId, equalTo(4));
+        assertThat(petId, equalTo(3));
     }
+
+    @Test
+    public void createPetWithFullJson_ShouldReturn200() {
+        String requestBody = """
+        {
+          "id": 10,
+          "name": "doggie",
+          "category": {
+            "id": 1,
+            "name": "Dogs"
+          },
+          "photoUrls": [
+            "string"
+          ],
+          "tags": [
+            {"id": 1,
+             "name": "tag2"
+             },
+             {
+              "id": 2,
+              "name": "tag3"
+             }
+          ],
+          "status": "available"
+        }
+        """;
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(10))
+                .body("name", equalTo("doggie"))
+                .body("status", equalTo("available"));
+    }
+
+
+
+
+
+
 
 }

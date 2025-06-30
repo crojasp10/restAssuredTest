bf4817a975d1c7bc6b0a151b14b3b50edd1dfe90
carlosalfredo.rojas <crojasw100@gmail.com>
2025-06-30 13:07:34 -0500

    develop/feature addying rest assured tests

--- src/test/java/api/petStore/PetApiTest.java
+++ src/test/java/api/petStore/PetApiTest.java
@@ -18,7 +18,7 @@ public class PetApiTest extends BaseTest {
           "id": 10,
           "name": "doggie",
           "category": {
-            "id": 1,
+            "id": 1.0,
             "name": "Dogs"
           },
           "photoUrls": [
@@ -76,5 +76,24 @@ public class PetApiTest extends BaseTest {
                 .then()
                 .statusCode(404);
     }
+
+    @Test
+    public void createPetValidationsReturn200() {
+        petClient.createPet(petObject).then().statusCode(200)
+                .body("id", equalTo(10))
+                .body("name", instanceOf(String.class))
+                .body("name.length()", allOf(greaterThanOrEqualTo(3), lessThanOrEqualTo(15)))
+                .body("category.id", instanceOf(Integer.class))
+                .body("category.name", instanceOf(String.class))
+                .body("photoUrls", not(empty()))
+                .body("tags", hasSize(greaterThanOrEqualTo(1)))
+                .body("tags[0].id", instanceOf(Integer.class))
+                .body("tags[0].name", instanceOf(String.class))
+                .body("status", equalTo("available"));
+
+    }
+
+
+
 
 }
